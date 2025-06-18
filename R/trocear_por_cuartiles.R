7baf62c15f8a587cb378c83a229a5aae4a66b88e
unknown <romenadanksnet@gmail.com>
2025-06-18 16:48:23 +0200

    añadir etiquetas en trocear por cuartiles

--- R/trocear_por_cuartiles.R
+++ R/trocear_por_cuartiles.R
@@ -4,6 +4,8 @@
 #' es asignado a un cuartil dependiendo de su valor.
 #'
 #' @param x Un vector numérico.
+#' @param añadirValor Por defecto FALSE. Si TRUE añade a la etiqueta el valor del cuartil.
+#' @param ... Parámetros de la función \code{analisisDescriptivo::formatear_numero}.
 #'
 #' @return Un vector con las etiquetas de cuartiles ('Cuartil_1', 'Cuartil_2', 'Cuartil_3', 'Cuartil_4')
 #'
@@ -12,13 +14,25 @@
 #'
 #' @examples
 #' trocear_por_cuartiles(c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-trocear_por_cuartiles <- function(x){
+#' trocear_por_cuartiles(mtcars$mpg)
+#' trocear_por_cuartiles(mtcars$mpg, añadirValor = T)
+trocear_por_cuartiles <- function(x, añadirValor = F,... ){
+
   cuartiles <- quantile(x,na.rm=T)
-  case_when(
-    x < cuartiles[2] ~ 'Cuartil_1',
-    x >= cuartiles[2] & x < cuartiles[3]  ~ 'Cuartil_2',
-    x >= cuartiles[3] & x < cuartiles[4]  ~ 'Cuartil_3',
-    x >= cuartiles[4] ~ 'Cuartil_4',
+  etiquetas <- paste0('Q',1:4)
+
+  if( añadirValor ){
+
+    cuartiles_format <- analisisDescriptivo::formatear_numero(cuartiles,...)
+
+    etiquetas <- paste0(etiquetas,' - ', cuartiles_format)
+  }
+
+    case_when(
+    x < cuartiles[2] ~ etiquetas[1],
+    x >= cuartiles[2] & x < cuartiles[3]  ~ etiquetas[2],
+    x >= cuartiles[3] & x < cuartiles[4]  ~ etiquetas[3],
+    x >= cuartiles[4] ~ etiquetas[4],
     is.na(x) == T ~ NA
   )
 }
